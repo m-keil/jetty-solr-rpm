e596d755682faff0296b01b30a47459e8d1d1d16
Boogie Shafer <boogie@ebrary.com>
2013-04-17 08:22:47 -0700

    change urls, add jetty and md5 downloads

--- build-jetty-solr.sh
+++ build-jetty-solr.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-version="4.2.0"
+solrversion="4.2.1"
+jettyversion="8.1.8.v20121106"
+
 rm -rf BUILD BUILDROOT tmp || true
 mkdir -p BUILD BUILDROOT RPMS SRPMS
 
-if [ ! -f SOURCES/solr-$version.tgz ];
+if [ ! -f SOURCES/solr-$solrversion.tgz ];
 then
-    wget "http://apache.mirrors.lucidnetworks.net/lucene/solr/$version/solr-$version.tgz" -O SOURCES/solr-$version.tgz
+    wget "http://www.us.apache.org/dist/lucene/solr/$solrversion/solr-$solrversion.tgz" -O SOURCES/solr-$solrversion.tgz
+    wget "http://www.us.apache.org/dist/lucene/solr/$solrversion/solr-$solrversion.tgz.md5" -O SOURCES/solr-$solrversion.tgz.md5
 fi
 
-rpmbuild -ba --target=noarch --define="_topdir $PWD" --define="_tmppath $PWD/tmp" --define="ver $version" jetty-solr.spec
+if [ ! -f SOURCES/jetty-distribution-$jettyversion.tar.gz ];
+then
+    wget "http://download.eclipse.org/jetty/$jettyversion/dist/jetty-distribution-$jettyversion.tar.gz" -O SOURCES/jetty-distribution-$jettyversion.tar.gz
+    wget "http://download.eclipse.org/jetty/$jettyversion/dist/jetty-distribution-$jettyversion.tar.gz.md5" -O SOURCES/jetty-distribution-$jettyversion.tar.gz.md5
+fi
+
+rpmbuild -ba --target=noarch --define="_topdir $PWD" --define="_tmppath $PWD/tmp" --define="sver $solrversion" --define="jver $jettyversion" jetty-solr.spec
